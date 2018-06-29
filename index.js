3df138a4c659d65d4d64340f4ca3f06952f3981b
raphsutti <neoster9@gmail.com>
2018-06-29 17:04:41 +1000

    testing root route

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ var express = require('express')
 
 var app = express()
 
+app.get('/', function(req, res) {
+  res.json({title: 'hello world'})
+})
+
 app.get('/notes/', function(req, res) {
   res.json({notes: "this is your notebook. Edit this to start saving your notes!"})
 })
 
-app.listen(3000)
+app.listen(3000, function () {
+  console.log('Server started on port 3000')
+})
